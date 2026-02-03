8684f12ea0e0f96650e6ca4986795c2cc5f28941
Samuel <assefa.samuel@gmail.com>
2026-02-03 16:13:17 +0300

    IMPORTANT: API updated to accomodate database access to server

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,13 +1,25 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_URL, {
+const sequelize = new Sequelize(
+  process.env.DB_NAME,      // Full Database Name
+  process.env.DB_USER,        // Full Username
+  process.env.DB_PASSWORD,           // The password you just created
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: 5432,
+    logging: console.log,   // This will show SQL in your logs
+  }
+);
+
+/*const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: 'postgres',
   logging: false, // Prevents leaking query data in logs
   define: {
     timestamps: true,
     underscored: true,
   },
-});
+});*/
 
 module.exports = sequelize;
